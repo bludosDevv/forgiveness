7056a15fcffec8b7f0f2ae1817879feef1a41233
BludosDev <bludosofficial+67@gmail.com>
2026-04-02 14:21:31 +0530

    Initial commit

---
+++ src/main/java/com/forgiveness/bludos/Main.java
@@ -0,0 +1,10 @@
+package com.forgiveness.bludos;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("forgiveness")
+public class Main {
+    public Main() {
+        System.out.println("Hello from forgiveness!");
+    }
+}
